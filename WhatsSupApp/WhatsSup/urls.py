979f5d4cd39da512a6b433780adbc1d3fa5eb734
djjoman21 <djjomail2@gmail.com>
2017-05-03 20:09:30 +0200

    refactor register, add login and logout

--- WhatsSupApp/WhatsSup/urls.py
+++ WhatsSupApp/WhatsSup/urls.py
@@ -5,7 +5,8 @@ from WhatsSup import views as app_view
 urlpatterns = [
     url(r'^$', app_view.glob, name="app"),
     url(r'^index/', app_view.index, name="index"),
-    url(r'^login/', app_view.login, name="login"),
+    url(r'^login/', app_view.login_view, name="login"),
+    url(r'^logout/', app_view.logout_view, name="logout"),
     url(r'^register/', app_view.register, name="register"),
     url(r'^information/', app_view.information, name="information"),
 ]
--- WhatsSupApp/WhatsSup/views.py
+++ WhatsSupApp/WhatsSup/views.py
@@ -10,8 +10,27 @@ def index(request):
     return render(request, 'index.html')
 
 
-def login(request):
-    return render(request, 'login.html')
+def login_view(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return HttpResponseRedirect('/')
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        pseudo = request.POST.get('pseudo', '')
+        password = request.POST.get('password', '')
+        user = authenticate(username=pseudo, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            return render(request, 'login.html', {'error': 'Veuillez vérifier votre pseudo ou mot de passe', 'form': request.POST})
+    else:
+        return HttpResponseRedirect('/')
+
+@login_required()
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
 
 
 def register(request):
@@ -27,26 +46,30 @@ def register(request):
         psw1 = request.POST.get('password1','')
         psw2 = request.POST.get('password2','')
         if not pseudo or not firstname or not lastname or not email or not psw1 or not psw2:
-            return render(request, 'register.html', {'error': 'Veuillez remplir tous les champs'})
+            return render(request, 'register.html', {'error': 'Veuillez remplir tous les champs', 'form': request.POST})
         try:
             user = User.objects.get(username=pseudo)
-            return render(request, 'register.html', {'error': 'Ce pseudo est déja utilisé'})
+            return render(request, 'register.html', {'error': 'Ce pseudo est déja utilisé', 'form': request.POST})
         except User.DoesNotExist:
             regex = re.compile(r"^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]+")
             if regex.match(email) is not None:
                 if psw1==psw2:
                     if len(psw1)>=6:
-                        user = User.objects.create_user(username=pseudo, lastname=lastname, firstname=firstname, email=email, password=psw1)
+                        User.objects.create_user(username=pseudo, last_name=lastname, first_name=firstname, email=email, password=psw1)
                         user = authenticate(username=pseudo, password=psw1)
-                        if user and user.is_active:
+                        if user is not None:
                             login(request, user)
                             return HttpResponseRedirect('/')
+                        else:
+                            return render(request, 'register.html',
+                                          {'error': 'Erreur lors le la connexion.',
+                                           'form': request.POST})
                     else:
-                        return render(request, 'register.html', {'error': 'Le mot de passe doit faire au moins 6 caractères'})
+                        return render(request, 'register.html', {'error': 'Le mot de passe doit faire au moins 6 caractères', 'form': request.POST})
                 else:
-                    return render(request, 'register.html', {'error': 'Les mots de passe de sont pas identiques'})
+                    return render(request, 'register.html', {'error': 'Les mots de passe de sont pas identiques', 'form': request.POST})
             else:
-                return render(request, 'register.html', {'error': 'L\'email n\'est pas valide'})
+                return render(request, 'register.html', {'error': 'L\'email n\'est pas valide', 'form': request.POST})
     else:
         return HttpResponseRedirect('/')
 
